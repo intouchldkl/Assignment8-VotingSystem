1fc5d6738573022c0d132985512ca29f74b474d5
ADMINS <ADMINS@DESKTOP-QFR4487>
2021-01-26 15:14:55 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Assignment8_VotingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vote[] votes = new Vote[1000000];
+
+            
+            
+
+            Console.WriteLine(Vote.getTotalVotes());
+
+            castVotes(votes);
+            
+
+        }
+
+        static void castVotes(Vote[] votes)
+        {
+            string name;
+            
+            int input = 0;
+            int i = 0;
+            Console.WriteLine("Hello What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Which party are you voting for?");
+            Console.WriteLine("1:Democrats  2:Communists  3:Facists  Press 0 to exit");
+            input = Console.Read();
+            while (input != 0)
+            {
+               
+                if(input == 1)
+                {
+                    votes[i] = new Vote(name,"Democrats");
+                    i = i + 1;
+                }
+                else if (input == 2)
+                {
+                    votes[i] = new Vote(name, "Communists");
+                    i = i + 1;
+                }
+                else if (input == 3)
+                {
+                    votes[i] = new Vote(name, "Facists");
+                    i = i + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Your input is ineligible");
+                }
+
+
+
+            }
+
+        }
+    }
+}
---
+++ Vote.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment8_VotingSystem
+{
+    class Vote
+    {
+        private string VoterName;
+        private string PartyName;
+        private DateTime TimeVoted;
+        private static int TotalVotes;
+
+        public Vote(string VoterName,string PartyName)
+        {
+            this.VoterName = VoterName;
+            this.PartyName = PartyName;
+            TimeVoted = DateTime.Now;
+            TotalVotes = TotalVotes + 1;
+
+        }
+
+        public string getPartyName()
+        {
+            return PartyName;
+        }
+
+        public static int getTotalVotes()
+        {
+            return TotalVotes;
+        }
+    }
+}
